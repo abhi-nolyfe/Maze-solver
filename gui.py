b1ed92870d5c4f3e13fd4a2ab9e955406f6a0e29
Abhi <abhi.nolyfe@gmail.com>
2024-02-02 21:51:11 +0530

    new file

---
+++ gui.py
@@ -0,0 +1,24 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+  def __init__(self, width, height):
+    self.__root = Tk()
+    self.__root.title("My Window")
+    self.__canvas = Canvas(self.__root, width=width, height=height)
+    self.__canvas.pack(fill=BOTH, expand=True)
+    self.__root.protocol("WM_DELETE_WINDOW", self.close)
+    self.__running = False
+
+
+  def redraw(self):
+    self.__root.update_idletasks()
+    self.__root.update()
+
+  def wait_for_close(self):
+    self.__running = True
+    while self.__running:
+      self.redraw()
+    print("closed")
+
+  def close(self):
+    self.__running = False
--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-from tkinter import Tk, BOTH, Canvas
-
-class Window():
-  def __init__(self, width, height):
-    self.__root = Tk()
-    self.__root.title("My Window")
-    self.__canvas = Canvas(self.__root, width=width, height=height)
-    self.__canvas.pack(fill=BOTH, expand=True)
-    self.__root.protocol("WM_DELETE_WINDOW", self.close)
-    self.__running = False
-
-
-  def redraw(self):
-    self.__root.update_idletasks()
-    self.__root.update()
-
-  def wait_for_close(self):
-    self.__running = True
-    while self.__running:
-      self.redraw()
-    print("closed")
-
-  def close(self):
-    self.__running = False
+from gui import Window
 
 def main():
   win = Window(800, 600)
