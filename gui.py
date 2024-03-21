e1af7ef0551df21e5cc6d6afb7a0e493be63a802
Abhi <abhi.nolyfe@gmail.com>
2024-03-21 21:06:40 +0530

    added cell class

--- gui.py
+++ gui.py
@@ -19,6 +19,27 @@ class Window():
     while self.__running:
       self.redraw()
     print("closed")
+  
+  def draw_line(self, line, fill_color="black"):
+    line.draw(self.__canvas, fill_color)
 
   def close(self):
     self.__running = False
+
+
+class Point():
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+  
+class Line():
+  def __init__(self, p1, p2):
+    self.p1 = p1
+    self.p2 = p2
+
+  def draw(self, canvas, fill_color="black"):
+    canvas.create_line(
+    self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+    )
+    canvas.pack(fill=BOTH, expand=True)
+
