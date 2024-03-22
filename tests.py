271dc98d3faebb3f091d36169ac2ee19e161b612
Abhi <abhi.nolyfe@gmail.com>
2024-03-22 18:35:13 +0530

    added tests for maze class

---
+++ tests.py
@@ -0,0 +1,32 @@
+import unittest
+from maze import Maze
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+    def test_maze_create_cells2(self):
+        num_cols = 20
+        num_rows = 20
+        m1 = Maze(50, 50, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+if __name__ == "__main__":
+    unittest.main()
